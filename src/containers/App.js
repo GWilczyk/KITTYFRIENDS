114b4b3cd4563d7f1385f27ae79219e036f2b1a1
GWilczyk <gwilczyk.github@gmail.com>
2020-11-21 21:37:32 +0100

    Deploy on Github-pages

---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import ErrorBoundry from '../components/ErrorBoundry';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			robots: [],
+			searchfield: ''
+		};
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({ robots: users }));
+	}
+
+	onSearchChange = event => {
+		this.setState({ searchfield: event.target.value });
+	};
+
+	render() {
+		const { robots, searchfield } = this.state;
+		const filteredRobots = robots.filter(robot =>
+			robot.name.toLowerCase().includes(searchfield.toLowerCase())
+		);
+		return !robots.length ? (
+			<h1>Loading…</h1>
+		) : (
+			<div className='tc'>
+				<h1 className='f1'>Kittyfriends</h1>
+				<SearchBox searchChange={this.onSearchChange} />
+				<Scroll>
+					<ErrorBoundry>
+						<CardList robots={filteredRobots} />
+					</ErrorBoundry>
+				</Scroll>
+			</div>
+		);
+	}
+}
+
+export default App;
